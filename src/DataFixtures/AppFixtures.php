fb171b17b877533388ebe4a02dcea5a3a1bdf263
Mokhtar <mokhtar.benyahia@hotmail.fr>
2022-12-15 21:52:57 +0100

    🚨🚨 maj   form recipe type en cours 🚨🚨

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use Faker\Generator;
+use App\Entity\Recipe;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -21,14 +22,35 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
+        // Ingredients
+        $ingredients = [];
         for ($i = 0; $i < 50; $i++) { 
             $ingredient = new Ingredient();
             $ingredient->setName($this->faker->word())
                 ->setPrice(mt_rand(0, 100));
 
+            $ingredients[] = $ingredient;
             $manager->persist($ingredient);     
         }
                               
+        //Recipes
+        for ($j=0; $j < 25 ; $j++) {
+            $recipe = new Recipe();
+            $recipe->setName($this->faker->word())
+                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : Null)
+                ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : Null)
+                ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : Null)
+                ->setDescription($this->faker->text(300))
+                ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : Null)
+                ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false);
+
+                for ($k=0; $k < mt_rand (5, 15); $k++) {
+
+                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
+                }
+
+            $manager->persist($recipe);
+        }
 
         $manager->flush();
     }
--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -6,12 +6,12 @@ use App\Entity\Ingredient;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
+
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException;
 
+use Symfony\Component\Validator\Constraints as Assert;
 
 class IngredientType extends AbstractType
 {
